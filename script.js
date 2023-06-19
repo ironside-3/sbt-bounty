b1ef02243bbe43e99c6de8bef53ec62d1bff1c2d
ironside-3 <135418624+ironside-3@users.noreply.github.com>
2023-06-19 19:39:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+function extractBattleId(battleLink) {
+    const url = new URL(battleLink);
+    const searchParams = new URLSearchParams(url.search);
+    return searchParams.get('id');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const leaderboardTable = document.getElementById('leaderboard');
+    const clearLogsBtn = document.getElementById('clearLogsBtn');
+    const battleLinks = new Set();
+
+    function updateLeaderboard(battleData) {
+        const { winner, player_1, player_2 } = battleData;
+
+        console.log('API Response:', battleData);
+
+        // Check if the necessary data is present
+        if (!winner || !player_1 || !player_2) {
+            console.log('Invalid battle data');
+            return;
+        }
+
+        // Retrieve existing leaderboard data from local storage
+        let leaderboardData = JSON.parse(localStorage.getItem('leaderboardData')) || [];
+
+        // Determine the winner and loser
+        let winningUsername, losingUsername;
+        if (winner === player_1) {
+            winningUsername = player_1;
+            losingUsername = player_2;
+        } else if (winner === player_2) {
+            winningUsername = player_2;
+            losingUsername = player_1;
+        } else {
+            console.log('Invalid winner');
+            return;
+        }
+
+        // Check if the winner exists in leaderboard data
+        const winnerIndex = leaderboardData.findIndex(data => data.username === winningUsername);
+        if (winnerIndex !== -1) {
+            leaderboardData[winnerIndex].wins += 1;
+        } else {
+            leaderboardData.push({ username: winningUsername, wins: 1, losses: 0 });
+        }
+
+        // Check if the loser exists in leaderboard data
+        if (losingUsername) {
+            const loserIndex = leaderboardData.findIndex(data => data.username === losingUsername);
+            if (loserIndex !== -1) {
+                leaderboardData[loserIndex].losses += 1;
+            } else {
+                leaderboardData.push({ username: losingUsername, wins: 0, losses: 1 });
+            }
+        }
+
+        // Update the leaderboard table
+        leaderboardTable.innerHTML = '';
+        leaderboardData.forEach(data => {
+            const row = leaderboardTable.insertRow();
+            const usernameCell = row.insertCell();
+            usernameCell.textContent = data.username;
+
+            const winCell = row.insertCell();
+            winCell.textContent = data.wins;
+
+            const lossCell = row.insertCell();
+            lossCell.textContent = data.losses;
+        });
+
+        // Store the updated leaderboard data in local storage
+        localStorage.setItem('leaderboardData', JSON.stringify(leaderboardData));
+    }
+
+    const battleForm = document.getElementById('battleForm');
+    const battleLinkInput = document.getElementById('battleLinkInput');
+
+    battleForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const battleLink = battleLinkInput.value;
+        const battleId = extractBattleId(battleLink);
+        if (battleId && !battleLinks.has(battleId)) { // Check if battleId exists and is not a duplicate
+            fetch(`https://api2.splinterlands.com/battle/result?id=${battleId}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch battle data');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    updateLeaderboard(data);
+                    battleLinks.add(battleId); // Add battleId to the Set to avoid duplicates
+                    battleLinkInput.value = ''; // Clear the input field
+                    location.reload(); // Refresh the page
+                })
+                .catch(error => {
+                    console.log('Error fetching battle data:', error);
+                });
+        } else {
+            console.log('Invalid battle link or duplicate entry');
+        }
+    });
+
+    clearLogsBtn.addEventListener('click', () => {
+        localStorage.removeItem('leaderboardData');
+        leaderboardTable.innerHTML = '';
+    });
+
+    // Retrieve existing leaderboard data from local storage and populate the leaderboard table
+    let leaderboardData = JSON.parse(localStorage.getItem('leaderboardData')) || [];
+    leaderboardData.forEach(data => {
+        const row = leaderboardTable.insertRow();
+        const usernameCell = row.insertCell();
+        usernameCell.textContent = data.username;
+
+        const winCell = row.insertCell();
+        winCell.textContent = data.wins;
+
+        const lossCell = row.insertCell();
+        lossCell.textContent = data.losses;
+
+        const roleCell = row.insertCell();
+        const roleDropdown = document.createElement('select');
+        roleDropdown.className = 'role-dropdown';
+        roleDropdown.addEventListener('change', (e) => {
+            data.role = e.target.value;
+            localStorage.setItem('leaderboardData', JSON.stringify(leaderboardData));
+        });
+        ['Legendary', 'Epic', 'Rare', 'Common', 'Wanted Person'].forEach(role => {
+            const option = document.createElement('option');
+            option.value = role;
+            option.textContent = role;
+            if (data.role === role) {
+                option.selected = true;
+            }
+            roleDropdown.appendChild(option);
+        });
+        roleCell.appendChild(roleDropdown);
+    });
+});
+
